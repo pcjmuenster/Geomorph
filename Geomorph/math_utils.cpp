f4bf4a6c18d4de2138dcd210b54c599f986eeb91
pcjmuenster <albjeno@yahoo.de>
2019-06-17 05:38:30 +0200

    add math utils
    implemented the functions in math_utils.cpp

--- Geomorph/math_utils.cpp
+++ Geomorph/math_utils.cpp
@@ -1,17 +1,19 @@
 #include "math_utils.h"
 
+#include <cmath>
+#include <limits>
 
 bool isZero(float f)
 {
-
+    return std::abs(f) < std::numeric_limits<float>::epsilon();
 }
 
 bool isEqual(float lhs, float rhs)
 {
-
+    return isZero(lhs - rhs);
 }
 
 float clamp(float min, float value, float max)
 {
-
+    return std::min(std::max(min, value), max);
 }
